5a8ac6c9fc992cc40c2828ea76eb05ac066c72b9
Dmitriy Bobir <astercassio@gmail.com>
2016-12-19 22:01:37 +0100

    Notebook with basic structure
    Add experiments

--- genetic_algorithms.py
+++ genetic_algorithms.py
@@ -20,7 +20,7 @@ class IndividualGA:
 
 
 class GeneticAlgorithms:
-    def __init__(self, data=None, fitness_func=None, selection="rank", mut_prob=0.5, mut_type=1,
+    def __init__(self, data=None, fitness_func=None, optim='max', selection="rank", mut_prob=0.05, mut_type=1,
                  cross_prob=0.95, cross_type=1, elitism=False, tournament_size=None):
         """
         Args:
@@ -28,6 +28,7 @@ class GeneticAlgorithms:
                 evaluated by a fitness function. Minimum amount of elements is 4.
             fitness_func (function): This function must compute fitness value of an input combination of the given data.
                 Function parameters must be: list of used indices of the given data (from 0), list of data itself.
+            optim (str): What an algorithm must do with fitness value: maximize or minimize. May be 'min' or 'max'.
             selection (str): Parent selection type. May be "rank" (Rank Wheel Selection),
                 "roulette" (Roulette Wheel Selection) or "tournament".
             tournament_size (int): Defines the size of tournament in case of 'selection' == 'tournament'.
@@ -42,6 +43,7 @@ class GeneticAlgorithms:
         """
         self.data = data
         self.fitness_func = fitness_func
+        self.optim = optim
         self.selection = selection
         self.tournament_size = tournament_size
         self.mutation_prob = mut_prob
@@ -57,6 +59,7 @@ class GeneticAlgorithms:
     def _check_parameters(self):
         if self.data is None or self.bin_length < 4 or \
                 self.fitness_func is None or \
+                self.optim not in ['min', 'max'] or \
                 self.mutation_prob < 0 or self.mutation_prob > 100 or \
                 self.mut_type < 1 or self.mut_type > self.bin_length or \
                 self.crossover_prob < 0 or self.crossover_prob > 100 or \
@@ -75,6 +78,7 @@ class GeneticAlgorithms:
             start (int): Start value of an interval (included). Default is 0.
             stop (int): End value of an interval (excluded).
             n (int): How many different random numbers generate.
+
         Returns:
              list of different random values from the given interval ('start' included)
         """
@@ -106,11 +110,12 @@ class GeneticAlgorithms:
         Args:
             individ (list): Binary encoded individual (it contains positions of bit 1 according to self.data).
             bit_num (list): List of bits' numbers to invert.
+
         Returns:
             mutated individual as binary representation (list)
         """
         for bit in bit_num:
-            if random.uniform(0, 100) <= self.mutation_prob:
+            if random.uniform(0, 1) <= self.mutation_prob:
                 # mutate
                 if bit in individ:
                     # 1 -> 0
@@ -128,6 +133,7 @@ class GeneticAlgorithms:
         Args:
             individ (list): Binary encoded combination of the original data list
                 (it contains positions of bit 1 according to self.data).
+
         Returns:
              mutated individual as binary representation (with inverted bits)
         """
@@ -151,6 +157,7 @@ class GeneticAlgorithms:
             target (list): Values in target are replaced with values in source.
             start (int): Start point of an interval (included).
             stop (int): End point of an interval (included).
+
         Returns:
              target with replaced bits with source one in the interval (start, stop) (both included)
         """
@@ -160,7 +167,7 @@ class GeneticAlgorithms:
             raise ValueError
 
         if start == stop:
-            if random.uniform(0, 100) <= self.crossover_prob:
+            if random.uniform(0, 1) <= self.crossover_prob:
                 # crossover
                 if start in source:
                     # bit 'start' is 1 in source
@@ -180,9 +187,9 @@ class GeneticAlgorithms:
             for index in source:
                 tmp_source[index] = 1
 
-            if random.uniform(0, 100) <= self.crossover_prob:
+            if random.uniform(0, 1) <= self.crossover_prob:
                 # crossover
-                tmp_target[start : stop+1] = tmp_source[start : stop+1]
+                tmp_target[start: stop+1] = tmp_source[start: stop+1]
 
             target = []
             for i in range(self.bin_length):
@@ -198,6 +205,7 @@ class GeneticAlgorithms:
         Args:
             parent1 (list): binary encoded combination of the original data list (self.data) of the first parent.
             parent2 (list): binary encoded combination of the original data list (self.data) of the second parent.
+
         Returns:
              list: an individual (binary representation) created by crossover of two parents
         """
@@ -239,28 +247,44 @@ class GeneticAlgorithms:
         Args:
             population (list): All possible competitors. Population element is an IndividualGA object.
             size (int): Size of a tournament.
+
         Returns:
             winners (tuple of IndividualGA): winner of current tournament and the second best participant
         """
-        if size > len(population) or size < 1:
+        population_size = len(population)
+
+        if size > population_size or size < 1:
             print('Wrong tournament size:', size)
             raise ValueError
 
-        competitors = self._random_diff(len(population), size)
-        # sort by fitness value in the ascending order
-        # and get the last two elements (winner and the second best participant)
-        competitors.sort(key=lambda x: population[x].fitness_val)
+        if size == population_size:
+            # sort by fitness value in the ascending order (maximization) or descending order (minimization)
+            if self.optim == 'max':
+                competitors = list(range(population_size))
+            else:
+                competitors = list(range(population_size - 1, 0, -1))
+        else:
+            competitors = self._random_diff(population_size, size)
+            # sort by fitness value in the ascending order (maximization) or descending order (minimization)
+            if self.optim == 'max':
+                # ascending order (maximization)
+                competitors.sort(key=lambda x: population[x].fitness_val)
+            else:
+                # descending order (minimization)
+                competitors.sort(key=lambda x: population[x].fitness_val, reverse=True)
 
+        # get the last two elements (winner and the second best participant)
         return competitors[-1], competitors[-2]
 
     def _select_parents(self, population, wheel_sum=None):
         """
-        Selects parents from the given population (sorted in ascending order).
+        Selects parents from the given population (sorted in ascending or descending order).
 
         Args:
-            population (list): Current population, sorted in ascending order, from which parents will be selected.
-                Population element is an IndividualGA object.
+            population (list): Current population, sorted in ascending or descending order,
+                from which parents will be selected. Population element is an IndividualGA object.
             wheel_sum (int): Sum of values on a wheel (different for "roulette" and "rank").
+
         Returns:
             parents (tuple of IndividualGA): selected parents
         """
@@ -271,24 +295,23 @@ class GeneticAlgorithms:
 
             parent1 = None
             parent2 = None
-            random1 = random.randrange(wheel_sum)
-            random2 = random.randrange(wheel_sum)
+            wheel1 = random.uniform(0, wheel_sum)
+            wheel2 = random.uniform(0, wheel_sum)
 
-            wheel = 0
-            for ind in population:
-                # population is sorted in ascending order
+            sum_val = 0
+            for ind, rank in zip(population, range(1, len(population) + 1)):
+                # population is sorted in ascending or descending order by fitness values
                 if self.selection == 'roulette':
-                    wheel += ind.fitness_val
+                    sum_val += ind.fitness_val
                 else:
-                    # each rank is greater by 1 than the previous one
-                    wheel += wheel + 1
+                    sum_val += rank
 
-                if random1 < wheel:
+                if parent1 is None and sum_val > wheel1:
                     parent1 = ind
-                if random2 < wheel:
+                if parent2 is None and sum_val > wheel2:
                     parent2 = ind
 
-                if parent1 and parent2:
+                if (parent1 is not None) and (parent2 is not None):
                     break
 
             return parent1, parent2
@@ -296,7 +319,7 @@ class GeneticAlgorithms:
             best1, second1 = self._conduct_tournament(population, self.tournament_size)
             best2, second2 = self._conduct_tournament(population, self.tournament_size)
 
-            if best1.individ == best2.individ:
+            if population[best1].individ == population[best2].individ:
                 return population[best1], population[second2]
             else:
                 return population[best1], population[best2]
@@ -314,6 +337,7 @@ class GeneticAlgorithms:
 
         Args:
             number (int): This decimal number represents binary encoded combination of the input data (self.data).
+
         Returns:
              list of positions with bit 1 (these positions are mapped on the input data list "as is" and thus,
              LSB is equal to index (self.bin_length - 1) of the input data list).
@@ -327,7 +351,14 @@ class GeneticAlgorithms:
         return binary_list
 
     def _sort_population(self):
-        self.population.sort(key=lambda x: x.fitness_val)
+        if self.optim == 'max':
+            # an algorithm maximizes a fitness value
+            # ascending order
+            self.population.sort(key=lambda x: x.fitness_val)
+        else:
+            # an algorithm minimizes a fitness value
+            # descending order
+            self.population.sort(key=lambda x: x.fitness_val, reverse=True)
 
     def init_population(self, new_population):
         """
@@ -364,7 +395,7 @@ class GeneticAlgorithms:
             else:
                 # 10% of all possible solutions
                 size = max_num // 10
-        elif 2 > size > self.bin_length:
+        elif size < 2 or size >= max_num:
             print('Wrong size of population:', size)
             raise ValueError
 
@@ -383,14 +414,23 @@ class GeneticAlgorithms:
     def run(self, max_generation):
         """
         Starts GA. The algorithm does 'max_generation' generations and then stops.
+        Old population is completely replaced with new one.
 
         Args:
             max_generation (int): Maximum number of GA generations.
+
+        Returns:
+            list of average fitness values for each generation (including original population)
         """
+        fitness_progress = []
+        fitness_sum = -1
+        population_size = None
+
         for generation_num in range(max_generation):
             fitness_sum = sum(ind.fitness_val for ind in self.population)
             population_size = len(self.population)
             next_population = []
+            fitness_progress.append(fitness_sum / population_size)
 
             for i in range(population_size):
                 if self.selection == 'roulette':
@@ -403,7 +443,9 @@ class GeneticAlgorithms:
                     # tournament
                     parent1, parent2 = self._select_parents(self.population)
 
+                # cross parents and mutate a child
                 new_individ = self._mutate(self._cross(parent1.individ, parent2.individ))
+                # compute fitness value of a child
                 fit_val = self.fitness_func(new_individ, self.data)
 
                 next_population.append(IndividualGA(new_individ, fit_val))
@@ -415,5 +457,7 @@ class GeneticAlgorithms:
             self.population = next_population
             self._sort_population()
 
+        fitness_progress.append(fitness_sum / population_size)
 
+        return fitness_progress
 
